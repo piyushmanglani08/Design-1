cb3920e1b4b7bc4d1ace82637b95db4fc528b58d
Piyush Manglani <piyushmanglani@Piyushs-MBP.lan>
2025-05-17 17:03:49 -0500

    complete

--- Problem_1.py
+++ Problem_1.py
@@ -1,22 +1,41 @@
-# Time Complexity : O(1)
-# Space Complexity : O(n) it will be the max size of the constraint given 
+# Time Complexity : add/remove/contains  = O(1)
+# Space Complexity : O(n) 
 # Did this code successfully run on Leetcode : Yes
-# Any problem you faced while coding this : 
-# I was trying to do it with 1000000 but changed it to 1000001 to fix the index issue 
 
-class MyHashMap:
+class MyHashSet:
 
     def __init__(self):
-        self.data = [None] * 1000001
-    
-    def put(self, key: int, value: int) -> None:
-        self.data[key] = value
+        self.bucket_count = 1000
+        self.bucket_size = 1000
+        self.storage = [None] * self.bucket_count
 
-    def get(self, key: int) -> int:
-        if self.data[key] is None:
-            return -1
-        else:
-            return self.data[key]
-        
-    def remove(self, key: int) -> None:
-        self.data[key] = None
+    def hash1(self, key):
+        return key % self.bucket_count
+
+    def hash2(self, key):
+        return key // self.bucket_size
+
+    def add(self, key):
+        bucket = self.hash1(key)
+        bucket_item = self.hash2(key)
+
+        if self.storage[bucket] is None:
+            if bucket == 0:
+                self.storage[bucket] = [False] * (self.bucket_size + 1)
+            else:
+                self.storage[bucket] = [False] * self.bucket_size
+
+        self.storage[bucket][bucket_item] = True
+
+    def remove(self, key):
+        bucket = self.hash1(key)
+        bucket_item = self.hash2(key)
+
+        if self.storage[bucket] is not None:
+            self.storage[bucket][bucket_item] = False
+
+    def contains(self, key):
+        bucket = self.hash1(key)
+        bucket_item = self.hash2(key)
+
+        return self.storage[bucket] is not None and self.storage[bucket][bucket_item]
--- Problem_2.py
+++ Problem_2.py
@@ -1,20 +1,29 @@
-# Time Complexity : O(n)
-# Space Complexity : O(1)
+# Time Complexity : push/pop/top/getmin = O(1)
+# Space Complexity : O(n)
 # Did this code successfully run on Leetcode : Yes
-# Any problem you faced while coding this : I was confused at first but figured out eventually
-# still have to change it to the right one 
+
 class MinStack:
     def __init__(self):
         self.stack = []
-        
+        self.min_stack = []
+
     def push(self, val: int) -> None:
-        return self.stack.append(val)
-        
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
     def pop(self) -> None:
-        return self.stack.pop()
+        if self.stack:
+            removed = self.stack.pop()
+            if removed == self.min_stack[-1]:
+                self.min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        if self.stack:
+            return self.stack[-1]
+        return None
 
     def getMin(self) -> int:
-        return min(self.stack)
+        if self.min_stack:
+            return self.min_stack[-1]
+        return None
